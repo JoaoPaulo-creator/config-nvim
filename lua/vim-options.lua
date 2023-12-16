e2184981fee389aec99db0a45f14d60eea1ff79f
JoaoPaulo-creator <joaosarto@gmail.com>
2023-12-16 16:56:58 -0300

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_set_keymap('n', '<leader>fp', 'vipgq', { noremap = true, silent = true })
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+  
+
